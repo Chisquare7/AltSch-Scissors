23d8ff8e4225c79e87ee86247d52092f84008311
Chisquare7 <chijiokechibuike.f@gmail.com>
2023-06-25 18:39:51 +0100

    Feat: modify the functionalities for accordions in the faq section.

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,26 @@
 // Few lines of js code for accordions
 
 let faqContainer = document.getElementById("faq__container");
+let accordions = faqContainer.getElementsByClassName("accordion");
 
 faqContainer.addEventListener("click", function (event) {
-	let clickedElement = event.target;
-	if (clickedElement.classList.contains("accordion")) {
-		clickedElement.classList.toggle("active");
-		var panel = clickedElement.nextElementSibling;
+	if (event.target.classList.contains("accordion")) {
+		var accordion = event.target;
+		var panel = accordion.nextElementSibling;
+
+		//Close all accordions
+
+		for (var i = 0; i < accordions.length; i++) {
+			var currentAccordion = accordions[i];
+			var currentPanel = currentAccordion.nextElementSibling;
+			if (currentAccordion !== accordion) {
+				currentAccordion.classList.remove("active");
+				currentPanel.style.display = "none";
+			}
+		}
+
+		// Toggle the clicked accordion
+		accordion.classList.toggle("active");
 		if (panel.style.display === "block") {
 			panel.style.display = "none";
 		} else {
@@ -15,9 +29,8 @@ faqContainer.addEventListener("click", function (event) {
 	}
 });
 
-
 // Few lines of code for hamburger functionalities
 
 function toggleMobileMenu(menu) {
-    menu.classList.toggle("open")
+	menu.classList.toggle("open");
 }
